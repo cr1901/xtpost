7563c62be5ce07e34d8a75f7e1e294157b2f9175
William D. Jones <thor0505@comcast.net>
2021-05-15 08:17:04 -0400

    Move cfg functions to own module.

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,4 +1,9 @@
+use directories::ProjectDirs;
 use serde::{Serialize, Deserialize};
+
+use std::fs::{create_dir_all, File};
+use std::io::prelude::*;
+use std::process::Command;
 
 const XT_SERVER: &str = "http://reenigne.mooo.com:8088/cgi-bin/xtserver.exe";
 
@@ -30,4 +35,38 @@ impl Default for Config {
             session_type: Default::default()
         }
     }
+}
+
+pub fn write_cfg_if_doesnt_exist() {
+    let cfg_dir = ProjectDirs::from("", "", "xtpost").unwrap().config_dir().to_path_buf();
+    let mut cfg_file = cfg_dir.clone();
+    cfg_file.push("settings.json");
+
+    if !cfg_dir.exists() {
+        create_dir_all(&cfg_dir).unwrap();
+    }
+
+    if !cfg_file.exists() {
+        let json_cfg = serde_json::to_string_pretty::<Config>(&Default::default()).unwrap();
+        let mut file = File::create(&cfg_file).unwrap();
+
+        file.write_all(&json_cfg.into_bytes()).unwrap();
+    }
+}
+
+pub fn open_editor() {
+    let cfg_dir = ProjectDirs::from("", "", "xtpost").unwrap().config_dir().to_path_buf();
+    let mut cfg_file = cfg_dir.clone();
+    cfg_file.push("settings.json");
+
+    let editor = if cfg!(target_os = "windows") {
+        "notepad"
+    } else {
+        "vi"
+    };
+
+    Command::new(editor)
+        .args(&[cfg_file])
+        .status()
+        .expect("editor failed to start");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,6 @@
-use directories::ProjectDirs;
-use serde_json;
-
-use std::fs::{create_dir_all, File};
-use std::io::prelude::*;
-use std::path::PathBuf;
-use std::process::Command;
-
 mod cfg;
 
 fn main() {
-    let cfg_dir = ProjectDirs::from("", "", "xtpost").unwrap().config_dir().to_path_buf();
-    let mut cfg_file = cfg_dir.clone();
-    cfg_file.push("settings.json");
-
-    if !cfg_dir.exists() {
-        create_dir_all(&cfg_dir).unwrap();
-    }
-
-    if !cfg_file.exists() {
-        let json_cfg = serde_json::to_string_pretty::<cfg::Config>(&Default::default()).unwrap();
-        let mut file = File::create(&cfg_file).unwrap();
-
-        file.write_all(&json_cfg.into_bytes()).unwrap();
-    }
-
-    open_editor(&cfg_file);
-}
-
-fn open_editor(cfg_file: &PathBuf) {
-    let editor = if cfg!(target_os = "windows") {
-        "notepad"
-    } else {
-        "vi"
-    };
-
-    Command::new(editor)
-        .args(&[cfg_file])
-        .status()
-        .expect("editor failed to start");
+    cfg::write_cfg_if_doesnt_exist();
+    cfg::open_editor();
 }
